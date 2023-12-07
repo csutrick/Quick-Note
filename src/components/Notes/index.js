2c0d6524d80bbfaf35900eca6a8ff438cc5d8a30
Cole Sutrick <papacole@Coles-MacBook-Pro-4.local>
2023-12-06 19:49:14 -0600

    Blocked out layout, added components

---
+++ src/components/Notes/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Notes = () => {
+    return (
+        <section className='bg-orange-300 h-full w-full flex justify-center items-center'>
+            <h1 className='font-bold text-4xl tracking-widest text-white'>NOTES</h1>
+        </section>
+    )
+};
+
+export default Notes;
---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Sidebar = () => {
+    return (
+        <section className='bg-green-300 h-full w-1/4 flex flex-col justify-center items-center'>
+            <h1 className='font-bold text-4xl tracking-widest text-white'>SIDEBAR</h1>
+        </section>
+    )
+};
+
+export default Sidebar;
