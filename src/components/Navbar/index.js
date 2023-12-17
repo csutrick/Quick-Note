3b6952641d0af3666a7d9f16d8b096567914a10f
Cole Sutrick <papacole@Coles-MacBook-Pro-4.local>
2023-12-16 19:59:16 -0600

    Added basic navbar with addNote button

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,9 +1,35 @@
 import React from "react";
 
-const Navbar = () => {
+import addNote from "../../util/createNewNote";
+
+import { MdNoteAdd } from "react-icons/md";
+
+const Navbar = ({ setAllNotes }) => {
+
+    {/* Add note to local storage */}
+    const createNewNote = () => {
+        const newNote = addNote();
+        setAllNotes(prevNotes => [newNote, ...prevNotes]);
+        console.log("New note added");
+    };
+
     return (
-        <nav className='z-50 bg-gray-400 p-4 flex flex-row justify-center items-center'>
-            <h1 className='font-bold text-4xl tracking-widest text-white'>NAVBAR</h1>
+        <nav className='z-40 bg-gray-400 flex flex-col justify-center items-center py-1'>
+            <h1 className='text-sm text-gray-600 font-bold tracking-widest'>Quick Notes</h1>
+            {/* Hold user buttons */}
+            <div className='flex flex-row justify-center items-center mt-1 mb-2'>
+                {/* Change format of user Note */}
+                <h2 className='mr-2 font-bold text-white'>Columns</h2>
+                {/* Create new note */}
+                <button onClick={createNewNote}
+                className='transition-all duration-100 ease-in-out
+                hover:scale-[1.1] active:scale-[1.35]'>
+                    <MdNoteAdd className='text-white text-4xl'/>
+                </button>
+                {/* Toggle light/dark mode */}
+                <h2 className='ml-2 font-bold text-white'>Theme</h2>
+            </div>
+            <h2 className='text-sm text-gray-600 font-bold tracking-widest'># Notes</h2>
         </nav>
     )
 };
